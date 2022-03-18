ab4538e2fa3685580fde2cf6d0c0653d40e972d5
vnuttens <84852873+vnuttens@users.noreply.github.com>
2022-03-18 11:19:18 +0100

    Update Lu-dose.py

--- Lu-dose.py
+++ Lu-dose.py
@@ -10,49 +10,77 @@ import lmfit
 # 24/02/2022
 #--------------------------------------------------------------------------
 
-# IN TE VULLEN
+# Select data file
+filename = "20220309_03.csv"    # file name met data
 
-A1KL = 0.02        # AT1/A0 in left kidney
-A2KL = 0.005       # AT2/A0 in left kidney
-A1KR = 0.018       # AT1/A0 in right kidney
-A2KR = 0.004       # AT2/A0 in right kidney
+#--------------------------------------------------------------------------
 
-T1 = 1         # days after administration (14h d0 en 8h d1 is  0.75)
-T2 = 7         # days after administration (14h d0 en 8h d7 is  6.75)
+# reading csv file
+rows = []
+with open(filename, 'r') as csvfile:
+        csvreader = csv.reader(csvfile,delimiter = ';')
+        # extracting each data row one by one
+        for row in csvreader:
+                rows.append(row)
+
+
+date = int(rows[1][1])
+EAD = int(rows[0][1])
+A0 = float(rows[8][1].replace(',', '.'))
+
+CF = float(rows[2][1].replace(',','.'))
+AT1 = int(rows[4][1].replace(',', '.'))
+AT2 = int(rows[34][1].replace(',', '.'))
+Vkl = float(rows[14][1].replace('Vol ','').replace(' ml','').replace(',','.'))
+Vsl = float(rows[20][1].replace('Vol ','').replace(' ml','').replace(',','.'))
+Vkr = float(rows[26][1].replace('Vol ','').replace(' ml','').replace(',','.'))
+Vsr = float(rows[32][1].replace('Vol ','').replace(' ml','').replace(',','.'))
+Csl1 = float(rows[19][1].replace('Tot ', '').replace(' CNTS','').replace(',','.'))
+Csl2 = float(rows[39][1].replace('Tot ', '').replace(' CNTS','').replace(',','.'))
+Csr1 = float(rows[31][1].replace('Tot ', '').replace(' CNTS','').replace(',','.'))
+Csr2 = float(rows[45][1].replace('Tot ', '').replace(' CNTS','').replace(',','.'))
+T1 = float(rows[3][1].replace(',','.'))
+T2 = float(rows[33][1].replace(',','.'))
+
+A1KL = Csl1/Vsl*Vkl/AT1/CF/A0
+A2KL = Csl2/Vsl*Vkl/AT2/CF/A0
+A1KR = Csr1/Vsr*Vkr/AT1/CF/A0
+A2KR = Csr2/Vsr*Vkr/AT1/CF/A0
+
+if not os.path.exists('Lu resultaten'):
+  os.mkdir('Lu resultaten')
 #--------------------------------------------------------------------------
 
 a = 0
-b = 8
+b = 400
 N = 1000
 t = np.linspace(a, b, N)
-Teff1 = 5
-Teff2 = 0.02
-Teff3 = 1
-Teff4 = 1
+Tinit1 = 0.9
+Tinit2 = 50
 
 x = [0,T1,T2]
 ykl = [0,A1KL,A2KL]
 ykr = [0,A1KR,A2KR]
 
-# Voor kidney, spleen, liver en speekselklier (?)
+# Voor kidney, spleen, liver en speekselklier
 model = lmfit.models.ExpressionModel("A1*(exp(-log(2)*x/Teff1) - exp(-log(2)*x/Teff2))")
-paramsKL = model.make_params(A1=A1KL, Teff1=Teff1, Teff2=Teff2)
-paramsKR = model.make_params(A1=A1KR, Teff1=Teff1, Teff2=Teff2)
+paramsKL = model.make_params(A1=A1KL, Teff1=Tinit1, Teff2=Tinit2)
+paramsKR = model.make_params(A1=A1KR, Teff1=Tinit1, Teff2=Tinit2)
 #params["A1"].set(vary=False)
 fitkl = model.fit(ykl, paramsKL, x=x)
-fitkr = model.fit(ykr, paramsKL, x=x)
+fitkr = model.fit(ykr, paramsKR, x=x)
 
 Teff1kl = fitkl.params["Teff1"].value
 Teff1kr = fitkr.params["Teff1"].value
 Teff2kl = fitkl.params["Teff2"].value
 Teff2kr = fitkr.params["Teff2"].value
-A1KL = fitkl.params["A1"].value
-A1KR = fitkr.params["A1"].value
+A1KLfit = fitkl.params["A1"].value
+A1KRfit = fitkr.params["A1"].value
 print(fitkl.fit_report())
 print(fitkr.fit_report())
 
-A12kl = A1KL*(np.exp(-np.log(2)*t/Teff1kl) - np.exp(-np.log(2)*t/Teff2kl))
-A12kr = A1KR*(np.exp(-np.log(2)*t/Teff1kr) - np.exp(-np.log(2)*t/Teff2kr))
+A12kl = A1KLfit*(np.exp(-np.log(2)*t/Teff1kl) - np.exp(-np.log(2)*t/Teff2kl))
+A12kr = A1KRfit*(np.exp(-np.log(2)*t/Teff1kr) - np.exp(-np.log(2)*t/Teff2kr))
 TIAC_KL = np.sum(A12kl)*(b-a)/N
 TIAC_KR = np.sum(A12kr)*(b-a)/N
 
@@ -64,18 +92,21 @@ TIAC_KR = np.sum(A12kr)*(b-a)/N
 
 fig1, ax1  = plt.subplots(1, 1, sharex=True,sharey=True)
 
-ax1.scatter(x,ykl, color='red',label='Kidney left data')
-ax1.scatter(x,ykr, color='blue',label='Kidney right data')
+ax1.scatter(x,ykl, color='red')
+ax1.scatter(x,ykr, color='blue')
 #ax1.plot(x,fit.init_fit, color='blue',label='Initiele curve')
 ax1.plot(t,A12kl, color='red',label=f'Kidney left '
                                       f'\nTeff1 = {"{:0.2f}".format(Teff1kl)}, Teff2 = {"{:0.2f}".format(Teff2kl)}'
-                                      f'\nA/A0 = {"{:0.2f}".format(TIAC_KL)}')
+                                      f'\nRes. Time = {"{:0.2f}".format(TIAC_KL)} h')
 
 ax1.plot(t,A12kr, color='blue',label=f'Kidney left '
                                       f'\nTeff1 = {"{:0.2f}".format(Teff1kr)}, Teff2 = {"{:0.2f}".format(Teff2kr)}'
-                                      f'\nA/A0 = {"{:0.2f}".format(TIAC_KR)}')
+                                      f'\nRes. time = {"{:0.2f}".format(TIAC_KR)} h')
 
-ax1.set_title('Time-activity curve (TAC)')
+ax1.set_title(f'EAD= {EAD} \nA0 = {"{:0.1f}".format(A0)} MBq, rt = {"{:0.2f}".format(TIAC_KL+TIAC_KR)} h, mass = {"{:0.2f}".format((Vkl+Vkr)*1.04)} g')
+ax1.set_xlim([-5, 200])
 ax1.set_ylabel('A(t)/A0')
-ax1.set_xlabel('Time [days]')
+ax1.set_xlabel('Time [hours]')
 ax1.legend()
+
+fig1.savefig(os.path.join('Lu resultaten',f'{date}_{EAD}.png'))
