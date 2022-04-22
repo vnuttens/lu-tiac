e2607bda51e7feaa2416702d42ce647e12c97de0
vnuttens <84852873+vnuttens@users.noreply.github.com>
2022-04-22 09:00:43 +0200

    Update Lu-dose.py

--- Lu-dose.py
+++ Lu-dose.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 import lmfit
+from fpdf import FPDF
 
 # Victor Nuttens
 # V03
@@ -11,7 +12,7 @@ import lmfit
 #--------------------------------------------------------------------------
 
 # Select data file
-filename = "20220309_03.csv"    # file name met data
+filename = "20220414_KP1.csv"    # file name met data
 
 #--------------------------------------------------------------------------
 
@@ -24,28 +25,29 @@ with open(filename, 'r') as csvfile:
                 rows.append(row)
 
 
+EAD  = int(rows[0][1])
 date = int(rows[1][1])
-EAD = int(rows[0][1])
-A0 = float(rows[8][1].replace(',', '.'))
+CF   = float(rows[2][1].replace(',','.'))
+T1   = float(rows[3][1].replace(',','.'))
+T2   = float(rows[4][1].replace(',','.'))
+AT1  = int(rows[5][1].replace(',', '.'))
+AT2  = int(rows[6][1].replace(',', '.'))
+A0   = float(rows[10][1].replace(',', '.'))
 
-CF = float(rows[2][1].replace(',','.'))
-AT1 = int(rows[4][1].replace(',', '.'))
-AT2 = int(rows[34][1].replace(',', '.'))
-Vkl = float(rows[14][1].replace('Vol ','').replace(' ml','').replace(',','.'))
-Vsl = float(rows[20][1].replace('Vol ','').replace(' ml','').replace(',','.'))
-Vkr = float(rows[26][1].replace('Vol ','').replace(' ml','').replace(',','.'))
-Vsr = float(rows[32][1].replace('Vol ','').replace(' ml','').replace(',','.'))
-Csl1 = float(rows[19][1].replace('Tot ', '').replace(' CNTS','').replace(',','.'))
-Csl2 = float(rows[39][1].replace('Tot ', '').replace(' CNTS','').replace(',','.'))
-Csr1 = float(rows[31][1].replace('Tot ', '').replace(' CNTS','').replace(',','.'))
-Csr2 = float(rows[45][1].replace('Tot ', '').replace(' CNTS','').replace(',','.'))
-T1 = float(rows[3][1].replace(',','.'))
-T2 = float(rows[33][1].replace(',','.'))
+Ckl1 = float(rows[17][1].replace('Tot ', '').replace(' CNTS','').replace(',','.'))
+Vkl1 = float(rows[18][1].replace('Vol ','').replace(' ml','').replace(',','.'))
+Ckr1 = float(rows[23][1].replace('Tot ', '').replace(' CNTS','').replace(',','.'))
+Vkr1 = float(rows[24][1].replace('Vol ','').replace(' ml','').replace(',','.'))
 
-A1KL = Csl1/Vsl*Vkl/AT1/CF/A0
-A2KL = Csl2/Vsl*Vkl/AT2/CF/A0
-A1KR = Csr1/Vsr*Vkr/AT1/CF/A0
-A2KR = Csr2/Vsr*Vkr/AT1/CF/A0
+Ckl2 = float(rows[30][1].replace('Tot ', '').replace(' CNTS','').replace(',','.'))
+Vkl2 = float(rows[31][1].replace('Vol ','').replace(' ml','').replace(',','.'))
+Ckr2 = float(rows[36][1].replace('Tot ', '').replace(' CNTS','').replace(',','.'))
+Vkr2 = float(rows[37][1].replace('Vol ','').replace(' ml','').replace(',','.'))
+
+A1KL = Ckl1/AT1/CF/A0
+A2KL = Ckl2/AT2/CF/A0
+A1KR = Ckr1/AT1/CF/A0
+A2KR = Ckr2/AT1/CF/A0
 
 if not os.path.exists('Lu resultaten'):
   os.mkdir('Lu resultaten')
@@ -55,8 +57,8 @@ a = 0
 b = 400
 N = 1000
 t = np.linspace(a, b, N)
-Tinit1 = 0.9
-Tinit2 = 50
+Tinit1 = 1
+Tinit2 = 1
 
 x = [0,T1,T2]
 ykl = [0,A1KL,A2KL]
@@ -83,6 +85,13 @@ A12kl = A1KLfit*(np.exp(-np.log(2)*t/Teff1kl) - np.exp(-np.log(2)*t/Teff2kl))
 A12kr = A1KRfit*(np.exp(-np.log(2)*t/Teff1kr) - np.exp(-np.log(2)*t/Teff2kr))
 TIAC_KL = np.sum(A12kl)*(b-a)/N
 TIAC_KR = np.sum(A12kr)*(b-a)/N
+totalmass1 = (Vkl1+Vkr1)*1.05
+totalmass2 = (Vkl2+Vkr2)*1.05
+masserror = np.abs(totalmass1-totalmass2)/totalmass2
+if masserror >0.3:
+    totaldose = "Massa tussen tijdstip 1 en 2 verschillen meer dan 20%"
+else:
+    totaldose = (TIAC_KL+TIAC_KR)*A0/totalmass1*0.204*422/1000
 
 # Voor red marrow
 # To do
@@ -90,7 +99,7 @@ TIAC_KR = np.sum(A12kr)*(b-a)/N
 #--------------------------------------------------------------------------
 # Making plot
 
-fig1, ax1  = plt.subplots(1, 1, sharex=True,sharey=True)
+fig1, ax1  = plt.subplots(1, 1, sharex=True,sharey=True,figsize=(11.69,8.27))
 
 ax1.scatter(x,ykl, color='red')
 ax1.scatter(x,ykr, color='blue')
@@ -99,14 +108,18 @@ ax1.plot(t,A12kl, color='red',label=f'Kidney left '
                                       f'\nTeff1 = {"{:0.2f}".format(Teff1kl)}, Teff2 = {"{:0.2f}".format(Teff2kl)}'
                                       f'\nRes. Time = {"{:0.2f}".format(TIAC_KL)} h')
 
-ax1.plot(t,A12kr, color='blue',label=f'Kidney left '
+ax1.plot(t,A12kr, color='blue',label=f'Kidney right '
                                       f'\nTeff1 = {"{:0.2f}".format(Teff1kr)}, Teff2 = {"{:0.2f}".format(Teff2kr)}'
                                       f'\nRes. time = {"{:0.2f}".format(TIAC_KR)} h')
 
-ax1.set_title(f'EAD= {EAD} \nA0 = {"{:0.1f}".format(A0)} MBq, rt = {"{:0.2f}".format(TIAC_KL+TIAC_KR)} h, mass = {"{:0.2f}".format((Vkl+Vkr)*1.04)} g')
+
+title = f'EAD= {EAD},A0 = {"{:0.1f}".format(A0)} MBq \nKidney dose = {"{:0.2f}".format(totaldose)} Gy'
+ax1.set_title(title)
 ax1.set_xlim([-5, 200])
 ax1.set_ylabel('A(t)/A0')
 ax1.set_xlabel('Time [hours]')
 ax1.legend()
 
-fig1.savefig(os.path.join('Lu resultaten',f'{date}_{EAD}.png'))
+fig1.savefig(os.path.join('Lu resultaten',f'{date}_{EAD}.pdf'))
+
+#print(f"SphereL {Csl1} \nKidneyL {Ckl1} \nSpheresR {Csr1} \nKidneyR {Ckr1}")
